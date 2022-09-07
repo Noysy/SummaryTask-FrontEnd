0c38d377e1efbf02502ddbae3d74ce06111afd05
noy <noy.yesodot@gmail.com>
2022-09-07 13:17:14 +0300

    fix: cleanup

--- src/Pages/PeoplePage.tsx
+++ src/Pages/PeoplePage.tsx
@@ -1,48 +1,48 @@
-import { useEffect, useState } from 'react';
-import { IPerson } from '../Interfaces/Person';
-import PeopleServices from '../Services/People';
-import CreatePerson from '../Components/People/CreatePerson';
-import { toast } from 'react-toastify';
-import AddButton from '../Components/AddButton';
-import Person from '../Components/People/Person';
+import { useEffect, useState } from "react";
+import { IPerson } from "../Interfaces/Person";
+import PeopleServices from "../Services/People";
+import CreatePerson from "../Components/People/CreatePerson";
+import { toast } from "react-toastify";
+import AddButton from "../Components/AddButton";
+import Person from "../Components/People/Person";
 
 const PeoplePage = () => {
-    const [peopleList, setPeopleList] = useState<IPerson[]>([]);
-    const [isCreating, setIsCreating] = useState(false);
+  const [peopleList, setPeopleList] = useState<IPerson[]>([]);
+  const [isCreating, setIsCreating] = useState(false);
 
-    useEffect(() => {
-        PeopleServices.getAllPeople()
-            .then((res) => {
-                setPeopleList(res.data);
-            })
-            .catch((err) => {
-                return toast.error(err.response.data);
-            });
-    }, [peopleList.length]);
+  useEffect(() => {
+    PeopleServices.getAllPeople()
+      .then((res) => {
+        setPeopleList(res.data);
+      })
+      .catch((err) => {
+        return toast.error(err.response.data);
+      });
+  }, [peopleList.length]);
 
-    const fullList = peopleList.map((person) => {
-        return (
-            <Person
-                key={person.id}
-                name={person.name}
-                favoriteColor={person.favoriteColor}
-                favoriteAnimal={person.favoriteAnimal}
-                favoriteFood={person.favoriteFood}
-                id={person.id}
-            />
-        );
-    });
-
+  const fullList = peopleList.map((person) => {
     return (
-        <div id="main-people">
-            {fullList}
-            {!isCreating ? (
-                <AddButton isCreating={isCreating} setIsCreating={setIsCreating} />
-            ) : (
-                <CreatePerson setIsCreating={setIsCreating} isCreating={isCreating} />
-            )}
-        </div>
+      <Person
+        key={person.id}
+        name={person.name}
+        favoriteColor={person.favoriteColor}
+        favoriteAnimal={person.favoriteAnimal}
+        favoriteFood={person.favoriteFood}
+        id={person.id}
+      />
     );
+  });
+
+  return (
+    <div id="main-people">
+      {fullList}
+      {!isCreating ? (
+        <AddButton isCreating={isCreating} setIsCreating={setIsCreating} />
+      ) : (
+        <CreatePerson setIsCreating={setIsCreating} isCreating={isCreating} />
+      )}
+    </div>
+  );
 };
 
 export default PeoplePage;
