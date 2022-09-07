37da802d0896b5d45cad9b821d95b639d490754e
noy <noy.yesodot@gmail.com>
2022-09-07 16:40:21 +0300

    feat: add new interfaces

--- src/Interfaces/Person.ts
+++ src/Interfaces/Person.ts
@@ -1,36 +1,54 @@
-export interface IPerson extends IAddPerson {
-    id: string;
+export interface IPerson {
+  name: string;
+  favoriteColor: string;
+  favoriteAnimal: string;
+  favoriteFood: string;
+  id: string;
+}
+
+export interface ICreatePerson {
+  name: string;
+  favoriteColor: string;
+  favoriteAnimal: string;
+  favoriteFood: string;
+  group: string;
+}
+
+export interface IPersonFunc extends IPerson {
+  setPeopleList: Function;
 }
 
 export interface IAutocomplete {
-    name: string;
-    id: string;
-}
-
-export interface IAddPerson {
-    name: string;
-    favoriteColor: string;
-    favoriteAnimal: string;
-    favoriteFood: string;
+  name: string;
+  id: string;
 }
 
 export interface IPersonDetails extends IPerson {
-    setAreSlotsEnabled: Function;
-    areSlotsEnabled: boolean;
+  setAreSlotsEnabled: Function;
+  areSlotsEnabled: boolean;
+}
+
+export interface personDisabledDetails extends IPersonDetails {
+  deletePerson: Function;
+}
+
+export interface personEnabledDetails extends IPersonDetails {
+  updatePerson?: Function;
+  createPerson?: Function;
 }
 
 export interface IPeopleProps {
-    setPeopleList: Function;
+  setPeopleList: Function;
 }
 
 export interface IPeoplePageProps {
-    peopleList: JSX.Element[];
+  peopleList: JSX.Element[];
 }
 
 export interface IUpdate {
-    name?: string;
-    favoriteColor?: string;
-    favoriteAnimal?: string;
-    favoriteFood?: string;
-    id: string;
+  name?: string;
+  favoriteColor?: string;
+  favoriteAnimal?: string;
+  favoriteFood?: string;
+  id: string;
 }
