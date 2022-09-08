b49280b30adbe0a96f76a0de486bc155638e5e9f
noy <noy.yesodot@gmail.com>
2022-09-08 17:06:07 +0300

    feat: add upload file function

--- src/Services/People.ts
+++ src/Services/People.ts
@@ -1,34 +1,44 @@
-import axios from 'axios';
-import configTs from '../config';
-import { ICreatePerson, IUpdate } from '../Interfaces/Person';
+import axios from "axios";
+import configTs from "../config";
+import { ICreatePerson, IUpdate } from "../Interfaces/Person";
 
 const url = `${configTs.URL}${configTs.PEOPLE_API}`;
 
 class PeopleServices {
-    static getAllPeople = async () => {
-        return await axios.get(`${url}/`);
-    };
+  static getAllPeople = async () => {
+    return await axios.get(`${url}/`);
+  };
 
-    static createPerson = async (personDetails: ICreatePerson) => {        
-        return await axios.post(`${url}/`, personDetails);
-    };
+  static createPerson = async (personDetails: ICreatePerson) => {
+    return await axios.post(`${url}/`, personDetails);
+  };
 
-    static updatePerson = async (personDetails: IUpdate) => {
-        return await axios({
-            method: 'patch',
-            url: `${url}/${personDetails.id}`,
-            data: {
-                name: personDetails.name,
-                favoriteColor: personDetails.favoriteColor,
-                favoriteAnimal: personDetails.favoriteAnimal,
-                favoriteFood: personDetails.favoriteFood,
-            },
-        });
-    };
+  static updatePerson = async (personDetails: IUpdate) => {
+    return await axios({
+      method: "patch",
+      url: `${url}/${personDetails.id}`,
+      data: {
+        name: personDetails.name,
+        favoriteColor: personDetails.favoriteColor,
+        favoriteAnimal: personDetails.favoriteAnimal,
+        favoriteFood: personDetails.favoriteFood,
+      },
+    });
+  };
 
-    static deletePerson = async (personId: string) => {
-        return await axios.delete(`${configTs.URL}/people/${personId}`);
-    };
+  static deletePerson = async (personId: string) => {
+    return await axios.delete(`${url}/${personId}`);
+  };
+
+  static uploadFile = async (
+    personId: string,
+    file: FormData,
+  ) => {
+    return await axios.put(
+      `${url}/addFileToPerson/${personId}/`,
+      file,
+    );
+  };
 }
 
 export default PeopleServices;
