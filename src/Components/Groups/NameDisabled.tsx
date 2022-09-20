5949059cf45cef3d0bf56d35526770ba81538f2b
noy <noy.yesodot@gmail.com>
2022-09-20 10:40:21 +0300

    feat: display edit button only if current role is admin

--- src/Components/Groups/NameDisabled.tsx
+++ src/Components/Groups/NameDisabled.tsx
@@ -1,45 +1,48 @@
-import { CardActions, Fab, Typography } from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { CardActions, Fab, Typography } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 interface INameProps {
-    setIsNameEditable: Function;
-    groupName: string;
-    deleteGroup: Function;
-    groupId: string;
+  setIsNameEditable: Function;
+  groupName: string;
+  deleteGroup: Function;
+  groupId: string;
   currentRole: string;
 }
 
 const NameDisabled = (props: INameProps) => {
-    const { setIsNameEditable, groupName, deleteGroup, groupId } = props;
+  const { setIsNameEditable, groupName, deleteGroup, groupId, currentRole } =
+    props;
 
-    return (
-        <>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                {groupName}
-            </Typography>
-            <CardActions>
-                <Fab
-                    color="default"
-                    aria-label="edit"
-                    size="medium"
-                    onClick={() => {
-                        setIsNameEditable((isNameEditable: boolean) => !isNameEditable);
-                    }}
-                >
-                    <EditIcon />
-                </Fab>
-                <Fab
-                    color="error"
-                    aria-label="delete"
-                    size="medium"
-                    onClick={() => deleteGroup(groupId, groupName)}
-                >
-                    <DeleteIcon />
-                </Fab>
-            </CardActions>
-        </>
-    );
+  return (
+    <>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        {groupName}
+      </Typography>
+      {currentRole === "ADMIN" && (
+        <CardActions>
+          <Fab
+            color="default"
+            aria-label="edit"
+            size="medium"
+            onClick={() => {
+              setIsNameEditable((isNameEditable: boolean) => !isNameEditable);
+            }}
+          >
+            <EditIcon />
+          </Fab>
+          <Fab
+            color="error"
+            aria-label="delete"
+            size="medium"
+            onClick={() => deleteGroup(groupId, groupName)}
+          >
+            <DeleteIcon />
+          </Fab>
+        </CardActions>
+      )}
+    </>
+  );
 };
 
 export default NameDisabled;
